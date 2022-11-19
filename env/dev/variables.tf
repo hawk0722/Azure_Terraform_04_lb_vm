87e688ce3d24f1d7196673a66bf419a6ebc13647
hawk0722 <siroinekotokuroineko@gmail.com>
2022-11-19 20:20:01 +0900

    add vm

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -5,3 +5,12 @@ variable "code" {}
 
 # vnet
 variable "cidr" {}
+
+# vm
+variable "vm_size" {}
+variable "vm_login_username" {}
+variable "vm_login_password" {}
+variable "vm_img_publisher" {}
+variable "vm_img_offer" {}
+variable "vm_img_sku" {}
+variable "vm_img_version" {}
--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -9,3 +9,27 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "vm-${var.env}-${var.code}"
+  resource_group_name = var.rg_name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.vm_login_username
+  admin_password      = var.vm_login_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = var.vm_img_publisher
+    offer     = var.vm_img_offer
+    sku       = var.vm_img_sku
+    version   = var.vm_img_version
+  }
+}
